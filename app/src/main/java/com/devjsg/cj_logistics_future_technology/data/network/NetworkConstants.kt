152937ea36fc0c05ea29f8279db0a0a12461dc74
SinnoLn <sintmtn69@gmail.com>
2024-11-10 20:57:47 +0900

    update setting

--- app/src/main/java/com/devjsg/cj_logistics_future_technology/data/network/NetworkConstants.kt
+++ app/src/main/java/com/devjsg/cj_logistics_future_technology/data/network/NetworkConstants.kt
@@ -1,6 +1,6 @@
 package com.devjsg.cj_logistics_future_technology.data.network
 
 object NetworkConstants {
-    const val BASE_URL = "http://10.0.2.2:8080/api/v1/"
-    //const val BASE_URL = "https://cj-api.serial-blog.com/api/v1/"
+    //const val BASE_URL = "http://10.0.2.2:8080/api/v1/"
+    const val BASE_URL = "https://smartworker.serial-blog.com/api/v1/"
 }
